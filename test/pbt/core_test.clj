596c0561ef4e632eae7c73ea33ebc37b612848bb
Michael Daines <953037+defndaines@users.noreply.github.com>
2019-03-02 11:30:01 -0800

    Adds Sanity Test: boolean Function

--- test/pbt/core_test.clj
+++ test/pbt/core_test.clj
@@ -1,7 +1,32 @@
 (ns pbt.core-test
-  (:require [clojure.test :refer :all]
-            [pbt.core :refer :all]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [clojure.test.check :as tc]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [pbt.core :as pbt]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+;; Sanity check of test.check
+
+(defn a-boolean [_] true)
+
+(defspec prop-boolean
+  100
+  (prop/for-all [x gen/any]
+    (a-boolean x)))
+
+
+(comment
+
+  (require '[clojure.test.check :as tc])
+  (require '[clojure.test.check.generators :as gen])
+  (require '[clojure.test.check.properties :as prop])
+
+  (def boolean-property
+    (prop/for-all [x gen/any]
+      (a-boolean x)))
+
+  (tc/quick-check 100 boolean-property)
+
+  )
