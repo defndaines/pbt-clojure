72976ba7f9b1d96dbe2c4fecef90f388d8e04590
Michael Daines <953037+defndaines@users.noreply.github.com>
2019-03-30 10:35:38 -0700

    Implement biggest using loop
    Chapter 2, page 28.
    
    Uses a `loop`-`recur` with a `cond` to simulate the Erlang approach,
    which introduces a bug at this point because of a missing equality case.
    
    Example failing test:
    ```
    lein test pbt.core-test
    {:result false
     :seed 1553967994214
     :failing-size 0
     :num-tests 1
     :fail [[2 2]]
     :shrunk {:total-nodes-visited 4, :depth 2, :result false, :smallest [[0]]}
     :test-var "prop-biggest"}
    
    lein test :only pbt.core-test/prop-biggest
    
    FAIL in (prop-biggest) (clojure_test.cljc:21)
    expected: result
      actual: false
    ```

--- src/pbt/core.clj
+++ src/pbt/core.clj
@@ -3,4 +3,9 @@
 (defn biggest
   "Find the biggest integer in a list."
   [v]
-  (first v))
+  (loop [[head & tail] v
+         big head]
+    (cond
+      (nil? head) big
+      (< big head) (recur tail head)
+      (< head big) (recur tail big))))
