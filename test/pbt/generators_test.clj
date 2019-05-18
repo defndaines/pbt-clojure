fbded46d7bdfba6fa40232f7ffe30b88eaf794a6
Michael Daines <953037+defndaines@users.noreply.github.com>
2019-05-18 10:35:08 -0700

    Demonstrating use of resize
    Chapter 4, page 62.
    
    Using sample to demonstrate how much longer a set of sample strings are
    when generated using the `resize` function to inject a different
    starting size.

--- test/pbt/generators_test.clj
+++ test/pbt/generators_test.clj
@@ -1,6 +1,7 @@
 (ns pbt.generators-test
   "Tests for Chapter 4: Custom Generators."
-  (:require [clojure.test.check.generators :as gen]
+  (:require [clojure.test :refer [deftest testing is]]
+            [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
             [clojure.test.check.clojure-test :refer [defspec]]
             #_[pbt.generators :as generators]))
@@ -18,3 +19,30 @@
         (fn key? [[k _v]]
           (get m k :not-found))
         kv)))))
+
+(defn avg-length
+  "Get the average length of a sequence of strings."
+  [s]
+  (float
+   (/ (reduce (fn [acc e] (+ acc (count e))) 0 s)
+      (count s))))
+
+(defn report-on-size
+  "Report, aka `println`, statistics about a sequence of strings."
+  [s]
+  (println
+   "Size:" (count s)
+   "Average:" (avg-length s)
+   "Smallest:" (apply min (map count s))
+   "Largest:" (apply max (map count s))))
+
+(deftest prop-resize
+  (testing "Use of resize to compare to a baseline size."
+    (let [size 100
+          base-strings (gen/sample gen/string size)
+          resized-strings (gen/sample (gen/resize 150 gen/string) size)]
+      (println "Difference between sample of strings with resize of 150 applied.")
+      (report-on-size base-strings)
+      (report-on-size resized-strings)
+      (is (< (avg-length base-strings)
+             (avg-length resized-strings))))))
